21f1c82729ce3e2bc78670682ca6e6d059c85aaa
Marcelo Leal <andradefj13@hotmail.com>
2024-01-07 20:46:49 -0300

    projectOneBitFlix
    Inicio do back do projeto,

---
+++ config/sequelizeCli.js
@@ -0,0 +1,10 @@
+module.exports = {
+    development: {
+        dialect:'postgres',
+        host: 'localhost',
+        port: 5432,
+        username: 'marcelo_sequelize',
+        password: '1010',
+        database: 'onebitflix'
+    }
+};
