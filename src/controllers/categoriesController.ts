03dbedd2ee7e987eefcb3ab42ea154d8b15cec83
Marcelo Leal <andradefj13@hotmail.com>
2024-01-12 18:27:53 -0300

    update
    Implementação do metodo GET da tabela de categorias que traz todos os cursos associados

--- src/controllers/categoriesController.ts
+++ src/controllers/categoriesController.ts
@@ -3,6 +3,7 @@
 import { Request, Response } from 'express'
 import { getPaginationParams } from '../helpers/getPaginationParams'
 import { categoryService } from '../services/categoryService'
+import { Category, Course } from '../models'
 
 const categoriesController = {
     index: async (req: Request, res: Response) => {
@@ -17,7 +18,26 @@ const categoriesController = {
                 return res.status(400).json({ message: err.message })
             }
         }
+    },
+
+    show: async (req: Request, res: Response) => {
+        const { id } = req.params
+
+        try {
+            const category = await Category.findByPk(id, {
+                include: {
+                    association: 'courses',
+                    attributes:['id','name','synopsis',['thumbnail_url','thumbnailUrl']]
+                },
+                attributes: ['id', 'name']
+            })
+            return res.json(category)
+        } catch (error) {
+            if (error instanceof Error) {
+                return res.status(400).json({ message: error.message })
+            }
+        }
     }
 }
 
-export { categoriesController }
+export {categoriesController}
