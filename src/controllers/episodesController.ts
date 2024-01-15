b749a0e8252acc84a8866ee599fd2f3c67da58aa
Marcelo Leal <andradefj13@hotmail.com>
2024-01-14 21:57:43 -0300

    update
    modificando o controller do episodes para manter a mesma organização dos outros

--- src/controllers/episodesController.ts
+++ src/controllers/episodesController.ts
@@ -1,50 +1,27 @@
 import { Request, Response } from "express";
-import fs from "fs";
-import path from "path";
+import { episodeService } from "../services/episodeService";
+
 
 
 export const episodesControler = {
     stream:async(req:Request, res:Response)=>{
+        const {videoUrl} = req.query
+
+
         try {
-            const {videoUrl} = req.query
             if(!videoUrl) throw new Error("videoUrl is required")
 
-            const filePath = path.join(__dirname, '..', '..', 'uploads', videoUrl.toString())
-            const fileStat = fs.statSync(filePath)
-
             const range = req.headers.range
-
-            if(range){
-                const parts = range.replace(/bytes=/, "").split("-")
-
-                const start = parseInt(parts[0], 10)
-                const end = parts[1] ? parseInt(parts[1], 10) : fileStat.size - 1
-
-                const chunksize = (end - start) + 1
-                const file = fs.createReadStream(filePath, {start, end})
-                
-                const head = {
-                    "Content-Range": `bytes ${start}-${end}/${fileStat.size}`,
-                    "Accept-Ranges": "bytes",
-                    "Content-Length": chunksize,
-                    "Content-Type": "video/mp4"
-                }
-                res.writeHead(206, head)
-
-                file.pipe(res)
-            }else{
-                const head = {
-                    "Content-Length": fileStat.size,
-                    "Content-Type": "video/mp4"
-                }
-                res.writeHead(200, head)
-                fs.createReadStream(filePath).pipe(res)
-            }
             
+            episodeService.streamEpisodeToResponse(res, videoUrl.toString(), range)
+
         } catch (error) {
             if(error instanceof Error) {
                 return res.status(400).json({message: error.message})
             }
         }
+
+
+
     }
 }
