ecabc2de60a7d4eae960725dcc4087ed24bec6fa
Marcelo Leal <andradefj13@hotmail.com>
2024-01-16 16:32:48 -0300

    update
    criação da tabela de associação de favoritos

--- src/models/index.ts
+++ src/models/index.ts
@@ -2,11 +2,20 @@ import { Category } from "./Category";
 import { Course } from "./Course";
 import { Episode } from "./Episode";
 import { User } from "./User";
+import { Favorite } from "./Favorite";
 
 Category.hasMany(Course, {as:'courses'})
 
 Course.belongsTo(Category)
 Course.hasMany(Episode, {as:'episodes'})
+Course.belongsToMany(User, {through:Favorite, as:'users', foreignKey:'course_id'})
+Course.hasMany(Favorite, {as:'FavoritesUsers', foreignKey:'course_id'})
+
+User.belongsToMany(Course, {through:Favorite, as:'courses', foreignKey:'user_id'})
+User.hasMany(Favorite, {as:'FavoritesCourses', foreignKey:'user_id'})
+
+Favorite.belongsTo(Course)
+Favorite.belongsTo(User)
 
 Episode.belongsTo(Course)
 
@@ -15,5 +24,6 @@ export {
     Category,
     Course,
     Episode,
-    User
+    User,
+    Favorite
 }
